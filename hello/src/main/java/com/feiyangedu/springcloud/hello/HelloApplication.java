cad02154c5bd298ac4a07f7fbba0c90c2b4549d5
SYL <1061300519@qq.com>
2018-05-27 00:10:18 +0800

    加入 Swagger2 调试接口工具

--- hello/src/main/java/com/feiyangedu/springcloud/hello/HelloApplication.java
+++ hello/src/main/java/com/feiyangedu/springcloud/hello/HelloApplication.java
@@ -1,37 +1,43 @@
 package com.feiyangedu.springcloud.hello;
 
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Spring Boot Application.
  * 
  * @author Michael Liao
  */
+@Controller
+@EnableAutoConfiguration
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class HelloApplication {
 
     /**
      * http://localhost:9090/
      * @return
      */
-	@GetMapping("/")
-	@ResponseBody
-	String home() {
-		return "<h1>Hello World!</h1>";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    String home() {
+        return "<h1>Hello World!</h1>";
+    }
 
     /**
      * http://localhost:9090/api/
      * @return
      */
-	@GetMapping("/api")
-	public String[] api() {
-		return "Hello Spring Boot Application".split(" ");
+    @RequestMapping(value = "/api", method = RequestMethod.GET)
+	public String[] api(@RequestParam(value="name") String name,@RequestParam(value="age", required=false) Integer age) {
+		return ("你好 name:"+name+" age:"+(age==null?0:age)+" 欢迎学习 Spring Boot Application").split(" ");
 	}
 
     /**
