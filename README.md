08c9b4fe08ef3771f1fb08c5cb035c3c01653af1
SYL <1061300519@qq.com>
2018-05-26 23:15:59 +0800

    spring cloud init application

---
+++ README.md
@@ -0,0 +1,5 @@
+# 这是springcloud学习的例子
+
+--------
+
+by *michaelliao*(https://github.com/michaelliao/springcloud)
---
+++ hello/src/main/java/com/feiyangedu/springcloud/hello/HelloApplication.java
@@ -0,0 +1,45 @@
+package com.feiyangedu.springcloud.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Boot Application.
+ * 
+ * @author Michael Liao
+ */
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+    /**
+     * http://localhost:9090/
+     * @return
+     */
+	@GetMapping("/")
+	@ResponseBody
+	String home() {
+		return "<h1>Hello World!</h1>";
+	}
+
+    /**
+     * http://localhost:9090/api/
+     * @return
+     */
+	@GetMapping("/api")
+	public String[] api() {
+		return "Hello Spring Boot Application".split(" ");
+	}
+
+    /**
+     * 第一个springCloud App
+     * @param args
+     * @throws Exception
+     */
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+}
